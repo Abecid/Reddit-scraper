bc6af72aecee0f7c9c44a3aa88ce4f602a2a7c2b
Adam <cmlee3211@gmail.com>
2023-09-26 01:01:07 -0700

    Add save video from imgur

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import os
+import re
 
 import praw
 from dotenv import load_dotenv
+from playwright.sync_api import sync_playwright
+
+from website_srape import general_video_scraper, imgur_video_scrape, download_youtube_video, imgur_video_save
 
 # Load variables from .env file into environment
 load_dotenv()
@@ -16,12 +20,40 @@ def main():
         client_secret=REDDIT_CILENT_SECRET,
         user_agent=REDDIT_USER_AGENT
     )
-    submission = reddit.submission(url="https://www.reddit.com/r/xxfitness/comments/119edqq/squat_form_check/")
-    post_title = submission.title
-    post_text = submission.selftext
     
-    print('post_title: ', post_title)
-    print('post_text: ', post_text)
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context(
+            user_agent="A common browser user agent",
+            accept_downloads=True,
+            viewport={'width': 1280, 'height': 800},
+            extra_http_headers={
+                'Accept-Language': 'en-US,en;q=0.9',
+                # Any other headers that a normal browser would send
+            }
+        )
+        page = context.new_page()
+        # page = browser.new_page()
+    
+        submission = reddit.submission(url="https://www.reddit.com/r/xxfitness/comments/119edqq/squat_form_check/")
+        post_title = submission.title
+        post_text = submission.selftext
+        
+        print('post_title: ', post_title)
+        print('post_text: ', post_text)
+        
+        match = re.search(r'https?://imgur\.com[a-zA-Z0-9/]*', post_text)
+        if match:
+            imgur_url = match.group(0)
+            print(imgur_url)
+            imgur_video_save(imgur_url)
+            # imgur_video_scrape(imgur_url, page)
+            # general_video_scraper(imgur_url)
+        
+        # if "https://imgur.com" in post_text:
+        #     imgur_url = post_text.split("https://imgur.com")[1].split()[0]
+        
+        browser.close()
     
     
 
--- website_srape.py
+++ website_srape.py
@@ -1,8 +1,85 @@
 import requests
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+import re
+import youtube_dl
 
-def website_video_scrape(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    video_tag = soup.find('video')
-    video_url = video_tag.source['src']
+def general_video_scraper(url):
+    try:
+        # Using youtube_dl to attempt video extraction
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': 'downloaded_video.%(ext)s'
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        print(f"Video downloaded from {url} using youtube_dl")
+
+    except:
+        print('Download with a specific video website scraper')
+        if 'imgur' in url:
+            imgur_video_scrape(url)
+        elif 'youtube' in url:
+            download_youtube_video(url)
+
+def imgur_video_save(url, save_path = "test_output/imgur_video.mp4"):
+    video_id = url.split('/')[-1]
+    
+    # Define the direct link to the video based on Imgur's pattern (this might change if Imgur updates their system)
+    video_url = f'https://i.imgur.com/{video_id}.mp4'
+
+    # Download the video
+    response = requests.get(video_url, stream=True)
+
+    if response.status_code == 200:
+        with open(save_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                file.write(chunk)
+        print(f"Video saved to {save_path}")
+    else:
+        print(f"Failed to download. Status code: {response.status_code}")
+
+# Deprecated
+def imgur_video_scrape(url, page):
+    page.goto(url)
+    
+    try:
+        # Wait for the video element to be loaded.
+        video_element = page.wait_for_selector('video > source', timeout=15000) # Timeout 3 seconds
+    except Exception as e:
+        print(e)
+        print("No video element found")
+        return
+    video_url = video_element.get_attribute('src')
+
+    if not video_url:
+        print("No video URL found")
+        return None
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    video_data = requests.get(video_url, headers=headers).content
+
+    name_from_url = url.split('/')[-1].split('.')[0]
+    
+    video_name = re.sub(r'[^a-zA-Z0-9]', '_', name_from_url)
+
+    video_data = requests.get(video_url, headers=headers).content
+    
+    with open(f"test_output/{video_name}.mp4", "wb") as f:
+        f.write(video_data)
+
+    print(f"Video downloaded from {url}")
+
+    # response = requests.get(url)
+    # soup = BeautifulSoup(response.content, 'html.parser')
+    # video_tag = soup.find('video')
+    # video_url = video_tag.source['src']
+    
+def download_youtube_video(youtube_url, output_path='.'):
+    ydl_opts = {
+        'format': 'best', # best format (usually mp4)
+        'outtmpl': f'{output_path}/%(title)s.%(ext)s',  # save file with video title as filename
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([youtube_url])
