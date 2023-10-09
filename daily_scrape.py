f1b7895c61ebb11b59ea16fd1e3d2831069347f4
Adam <cmlee3211@gmail.com>
2023-10-08 21:28:53 -0700

    Init  daily  scrape

--- daily_scrape.py
+++ daily_scrape.py
@@ -21,12 +21,12 @@ def check_post_posted_recently(posted_datetime, days=7):
     # Check if the post was made within the last 7 days
     is_within_week = (current_datetime_utc - posted_datetime) < datetime.timedelta(days=days)
 
-    print(is_within_week)  #
+    return is_within_week  #
 
-def get_updated_comments(submission, subreddit_name, output_path, post_data, original_json):
+def get_updated_comments(submission, original_json):
     posted_datetime = submission.created_utc
     
-    if check_post_posted_recently():
+    if check_post_posted_recently(posted_datetime):
         comments_data = []
         for comment in submission.comments:
             if not isinstance(comment, praw.models.MoreComments):
@@ -42,7 +42,11 @@ def get_updated_comments(submission, subreddit_name, output_path, post_data, ori
             highest_reated_comment = max(comments_data, key=lambda x: x["upvotes"] - x["downvotes"])
             
             comments_data = sorted(comments_data, key=lambda x: x["upvotes"] - x["downvotes"], reverse=True)
+        
+        # if len(comments_data) > len(original_json['Comments']):
         original_json['Comments'] = comments_data
+        original_json['Updated'] = True
+        original_json['Last Update Date'] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         return original_json
     else:
         return original_json
@@ -51,7 +55,8 @@ def get_updated_sumbission(submission, subreddit_name, output_path, post_data):
     if not post_exists(submission.id, post_data):
         return get_submission(submission, subreddit_name, output_path, post_data)
     else:
-        return get_updated_comments(submission, subreddit_name, output_path, post_data)
+        original_json = [item for item in post_data if item.get("Post ID", None) == submission.id][0]
+        return get_updated_comments(submission, original_json)
 
 def save_daily_scrape(input_filename, reddit, output_path="output"):
     input_data = json.load(open(f'input/{input_filename}'))
@@ -67,35 +72,6 @@ def save_daily_scrape(input_filename, reddit, output_path="output"):
         # subreddit = reddit.subreddit("SUBREDDIT_NAME")
         subreddit = reddit.subreddit(subreddit_name)
         
-        sort_types = [
-            {
-                "name": "top",
-                "func": subreddit.top,
-                "time_filter": 'all'
-            }
-            ,
-            {
-                "name": "new",
-                "func": subreddit.new,
-                "time_filter": False
-            }, 
-            {
-                "name": "hot",
-                "func": subreddit.hot,
-                "time_filter": False
-            }, 
-            {
-                "name": "controversial",
-                "func": subreddit.controversial,
-                "time_filter": 'all'
-            },
-            {
-                "name": "rising",
-                "func": subreddit.rising,
-                "time_filter": False
-            }
-        ]
-        
         post_data = []
         post_data = load_existing_data(f'{output_path}/subreddits/{subreddit_name}/submissions.json')
         
@@ -108,35 +84,45 @@ def save_daily_scrape(input_filename, reddit, output_path="output"):
             video_links_saved_json = [{"Post ID": item["Post ID"], "External url": item["External url"]} for item in video_links_saved_json]
             videos_links_saved_failed_json = external_video_info.get("Videos failed to save from external links", [])
             videos_links_saved_failed_json = [{"Post ID": item["Post ID"], "External url": item["External url"]} for item in videos_links_saved_failed_json]
-    for submission in tqdm(subreddit.top(limit=subreddit_json.get("max", 10), time_filter='all')):  # Adjust the limit as needed
-        # Check if the post has a video
-        submission_json = get_updated_sumbission(submission, subreddit_name, output_path, post_data)
-        if submission_json is not None:
-            if submission_json.get("Video Failed", False) is False: 
-                post_data.append(submission_json)
-                if submission_json.get("External url", None) is not None:
-                    # video_links_saved_json.append(submission_json)
-                    video_links_saved_json.append(get_keys_in_json(submission_json, ["Post ID", "External url"]))
-            else: videos_links_saved_failed_json.append(submission_json)
-    
+        for submission in tqdm(subreddit.new(limit=subreddit_json.get("max", 10), time_filter='all')):  # Adjust the limit as needed
+            # Check if the post has a video
+            submission_json = get_updated_sumbission(submission, subreddit_name, output_path, post_data)
+            if submission_json is not None:
+                if submission_json.get("Video Failed", False) is False: 
+                    # Update the original json not add new one
+                    if submission_json.get("Updated", False) is True:
+                        for index, item in enumerate(post_data):
+                            if item.get("Post ID", None) == submission_json.get("Post ID", None):
+                                post_data[index] = submission_json
+                                break
+                    else:
+                        post_data.append(submission_json)
+                #         if submission_json.get("External url", None) is not None:
+                #             # video_links_saved_json.append(submission_json)
+                #             video_links_saved_json.append(get_keys_in_json(submission_json, ["Post ID", "External url"]))
+                # else: videos_links_saved_failed_json.append(submission_json)
     filepath = f'{output_path}/subreddits/{subreddit_name}/submissions.json'
     save_data_to_file(post_data, filepath)
-    subreddit_info_object = {
-        "length": len(post_data),
-        "sort types completed": idx+1
-    }
+    
     subreddit_info_filepath = f'{output_path}/subreddits/{subreddit_name}/info.json'
+    subreddit_info_object = load_existing_data(subreddit_info_filepath)
+    subreddit_info_object["length"] = len(post_data)
+    subreddit_info_object["Last Update Date"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     save_data_to_file(subreddit_info_object, subreddit_info_filepath)
     
-    subreddit_video_in_link_info = {
-        "Total Videos Saved from External Links": len(video_links_saved_json),
-        "Total Videos failed to save from external links": len(videos_links_saved_failed_json),
-        "Last sort type": "new",
-        "Videos saved from external links": video_links_saved_json,
-        "Videos failed to save from external links": videos_links_saved_failed_json
-    }
-    subreddit_video_in_link_info_path = f'{output_path}/subreddits/{subreddit_name}/video_link_info.json'
-    save_data_to_file(subreddit_video_in_link_info, subreddit_video_in_link_info_path)
+    # subreddit_video_in_link_info = {
+    #     "Total Videos Saved from External Links": len(video_links_saved_json),
+    #     "Total Videos failed to save from external links": len(videos_links_saved_failed_json),
+    #     "Last sort type": "new",
+    #     "Videos saved from external links": video_links_saved_json,
+    #     "Videos failed to save from external links": videos_links_saved_failed_json
+    # }
+    
+    # subreddit_video_in_link_info_path = f'{output_path}/subreddits/{subreddit_name}/video_link_info.json'
+    # subreddit_video_in_link_info = load_existing_data(subreddit_video_in_link_info_path)
+    # subreddit_video_in_link_info["Total Videos Saved from External Links"] = len(video_links_saved_json)
+    # subreddit_video_in_link_info["Total Videos failed to save from external links"] = len(videos_links_saved_failed_json)
+    # save_data_to_file(subreddit_video_in_link_info, subreddit_video_in_link_info_path)
 
 def main():
     pass
